127cb059fd63316f4c367946b0566ab3e1e082db
zoomdong <1344492820@qq.com>
2020-05-09 21:37:51 +0800

    fix: exercies04

--- exercises/exercise-04/index.ts
+++ exercises/exercise-04/index.ts
@@ -95,11 +95,7 @@ function logPerson(person: Person) {
     console.log(` - ${chalk.green(person.name)}, ${person.age}, ${additionalInformation}`);
 }
 
-type Choosen<T> = {
-    [P in keyof T]?: T[P];
-}
-
-function filterUsers(persons: Person[], criteria: Choosen<User>): User[] {
+function filterUsers(persons: Person[], criteria: Partial<User>): User[] {
     return persons.filter(isUser).filter((user) => {
         let criteriaKeys = Object.keys(criteria) as (keyof User)[];
         return criteriaKeys.every((fieldName) => {
