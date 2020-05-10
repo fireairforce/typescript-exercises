47b54004be4ef81c1ffc592a90c6aaa6b062f847
zoomdong <1344492820@qq.com>
2020-05-10 15:49:11 +0800

    fix: update exercise 12

--- exercises/exercise-12/module-augmentations/date-wizard/index.ts
+++ exercises/exercise-12/module-augmentations/date-wizard/index.ts
@@ -3,4 +3,11 @@ import 'date-wizard';
 
 declare module 'date-wizard' {
     // Add your module extensions here.
+    interface DateDetails {
+        hours: number;
+        minutes: number;
+        seconds: number;
+    }
+
+    export const pad: (s: number) => string
 }
