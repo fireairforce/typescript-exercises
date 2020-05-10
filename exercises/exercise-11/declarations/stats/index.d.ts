25e9935e92c183767915f7dd094d3446f2f997b8
zoomdong <1344492820@qq.com>
2020-05-10 15:35:25 +0800

    feat: finish exercise11

--- exercises/exercise-11/declarations/stats/index.d.ts
+++ exercises/exercise-11/declarations/stats/index.d.ts
@@ -1,3 +1,13 @@
 declare module 'stats' {
-    export function getMaxIndex(input: unknown, comparator: unknown): unknown;
+    type Comparator<T> = (a: T, b: T) => number
+    type IndexFunction = <T>(input: Array<T>, comparator: Comparator<T>) => number
+    type ElementFunction = <T>(input: Array<T>, comparator: Comparator<T>) => T
+
+    export const getMaxIndex: IndexFunction;
+    export const getMaxElement: ElementFunction;
+    export const getMinIndex: IndexFunction;
+    export const getMinElement: ElementFunction;
+    export const getMedianIndex: IndexFunction;
+    export const getMedianElement: ElementFunction;
+    export const getAverageValue: <T, U>(input: Array<T>, getValue: (item: T) => U) => U
 }
